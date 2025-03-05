0acd79a71ca96e136d87d680cb3714a50092cb1d
ramialmallahi <raminazmi7@gmail.com>
2025-03-05 09:32:05 +0200

    commit "7"

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost', 'https://36rwrd.netlify.app'],
+    'allowed_origins' => ['http://localhost', 'localhost:5173', 'https://36rwrd.netlify.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
