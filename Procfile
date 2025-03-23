f6d8d7d37362545fd415d8cef926169897592f37
ramialmallahi <raminazmi7@gmail.com>
2025-03-23 20:38:32 +0200

    commit "38"

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: php -S 0.0.0.0:$PORT public/index.php
+scheduler: php run-schedule.php
--- run-schedule.php
+++ run-schedule.php
@@ -1,16 +1,9 @@
 <?php
 while (true) {
-    $currentHour = date('H');
-    if ($currentHour == '00') {
-        $output1 = shell_exec('php artisan subscriptions:check');
-        if ($output1 === null) {
-            file_put_contents('scheduler.log', date('Y-m-d H:i:s') . " - Error executing subscriptions:check\n", FILE_APPEND);
-        }
+    $output1 = shell_exec('php artisan subscriptions:check');
+    $output2 = shell_exec('php artisan subscriptions:notify-renewal');
 
-        $output2 = shell_exec('php artisan subscriptions:notify-renewal');
-        if ($output2 === null) {
-            file_put_contents('scheduler.log', date('Y-m-d H:i:s') . " - Error executing subscriptions:notify-renewal\n", FILE_APPEND);
-        }
-    }
+    file_put_contents('scheduler.log', date('Y-m-d H:i:s') . " - subscriptions:check output:\n$output1\n", FILE_APPEND);
+    file_put_contents('scheduler.log', date('Y-m-d H:i:s') . " - subscriptions:notify-renewal output:\n$output2\n", FILE_APPEND);
     sleep(1);
 }
