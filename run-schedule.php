a404e0f50f714d26b9ad4a8fa0cfb656ab640dac
ramialmallahi <raminazmi7@gmail.com>
2025-03-23 21:10:56 +0200

    commit "39"

--- run-schedule.php
+++ run-schedule.php
@@ -1,9 +1,21 @@
 <?php
 while (true) {
-    $output1 = shell_exec('php artisan subscriptions:check');
-    $output2 = shell_exec('php artisan subscriptions:notify-renewal');
+    $targetHour = '00';
+    $currentHour = date('H');
 
-    file_put_contents('scheduler.log', date('Y-m-d H:i:s') . " - subscriptions:check output:\n$output1\n", FILE_APPEND);
-    file_put_contents('scheduler.log', date('Y-m-d H:i:s') . " - subscriptions:notify-renewal output:\n$output2\n", FILE_APPEND);
-    sleep(1);
+    if ($currentHour == $targetHour) {
+        $output1 = shell_exec('php artisan subscriptions:check');
+        $output2 = shell_exec('php artisan subscriptions:notify-renewal');
+
+        file_put_contents(
+            'scheduler.log',
+            date('Y-m-d H:i:s') . " - subscriptions:check output:\n$output1\n" .
+                date('Y-m-d H:i:s') . " - subscriptions:notify-renewal output:\n$output2\n",
+            FILE_APPEND
+        );
+
+        sleep(3600 * 23);
+    } else {
+        sleep(1800);
+    }
 }
